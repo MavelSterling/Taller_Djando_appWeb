659b9e7a8215e5a113103f193e379d454094353f
MavelSterling <mavelynsl@gmail.com>
2023-09-29 20:07:35 -0500

    Update models.py

--- univalle_project/univalle_app/models.py
+++ univalle_project/univalle_app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.core.exceptions import ValidationError
+from datetime import time
 
 class Univalluno(models.Model):
     ESTUDIANTE = 'Estudiante'
@@ -42,21 +43,28 @@ class ArticuloDeportivo(models.Model):
         return self.nombre
 
 
-
 class Prestamo(models.Model):
     univalluno = models.ForeignKey(Univalluno, on_delete=models.CASCADE)
     articulo_deportivo = models.ForeignKey(ArticuloDeportivo, on_delete=models.SET_NULL, null=True)
-    fecha_prestamo = models.DateTimeField(auto_now_add=True)
+    fecha_prestamo = models.DateTimeField(auto_now_add=True)  # Se establece automáticamente al crear un nuevo registro
     fecha_vencimiento = models.DateTimeField()
 
     def clean(self):
         # Verificar si el Univalluno ya tiene un artículo en préstamo
         prestamo_existente = Prestamo.objects.filter(univalluno=self.univalluno, articulo_deportivo__prestado=True).exclude(pk=self.pk)
-        
         if prestamo_existente.exists():
             raise ValidationError("El Univalluno ya tiene un artículo en préstamo.")
 
+        # Verificar si el Univalluno tiene multas pendientes
+        multas_pendientes = Multa.objects.filter(prestamo__univalluno=self.univalluno, pagada=False)
+        if multas_pendientes.exists():
+            raise ValidationError("El Univalluno tiene multas pendientes. Debe pagarlas antes de solicitar otro préstamo.")
+
     def save(self, *args, **kwargs):
+        # Si es una nueva instancia, establece la fecha_vencimiento a las 8:00 pm del día actual
+        if not self.pk:
+            self.fecha_vencimiento = datetime.combine(self.fecha_prestamo.date(), time(20, 0))
+
         self.clean()
         super(Prestamo, self).save(*args, **kwargs)
 
@@ -64,11 +72,34 @@ class Prestamo(models.Model):
         return f"Prestamo {self.id} - {self.univalluno} - {self.articulo_deportivo}"
 
 
+
 class Multa(models.Model):
     prestamo = models.OneToOneField(Prestamo, on_delete=models.CASCADE)
     valor_multa = models.PositiveIntegerField()
     pagada = models.BooleanField(default=False)
     fecha_pago = models.DateTimeField(null=True, blank=True)
+
+    def pagar(self):
+        # Marca la multa como pagada, registra la fecha de pago y libera el artículo deportivo.
+        self.pagada = True
+        self.fecha_pago = timezone.now()
+        # Liberar el artículo deportivo
+        articulo = self.prestamo.articulo_deportivo
+        articulo.prestado = False
+        articulo.save()
+        self.save()
+        
+    def generar_multas():
+        # Obten todos los préstamos que no han sido devueltos antes de las 8:10 pm
+        ahora = timezone.now()
+        prestamos_pendientes = Prestamo.objects.filter(fecha_vencimiento__lt=ahora, articulo_deportivo__prestado=True)
+    
+        for prestamo in prestamos_pendientes:
+            # Aquí puedes calcular el valor de la multa basado en tus reglas de negocio
+            valor_multa = calcular_valor_multa(prestamo)
+        
+            Multa.objects.create(prestamo=prestamo, valor_multa=valor_multa)
+
 
     def __str__(self):
         return f"Multa {self.id} - Prestamo {self.prestamo.id}"
