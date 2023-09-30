3239664b41cfca730857f7d40b1151dacc70cc57
MavelSterling <mavelynsl@gmail.com>
2023-09-30 00:28:33 -0500

    Update urls.py

--- univalle_project/univalle_project/urls.py
+++ univalle_project/univalle_project/urls.py
@@ -16,14 +16,42 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from univalle_app.views import generar_multas_view
-
+#from univalle_app.views import generar_multas_view
+#from univalle_app.views import datos_articulos_por_deporte
+from univalle_app import views
+from django.shortcuts import render
+from django.urls import path, re_path, include
+from django.shortcuts import redirect
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path('generar_multas/', generar_multas_view, name='generar_multas'),
+    re_path(r'^$', lambda request: redirect('univalluno-list', permanent=False)),
+
+    # Vistas CRUD para Univalluno
+    path('univalluno/', views.UnivallunoList.as_view(), name='univalluno-list'),
+    path('univalluno/<int:pk>/', views.UnivallunoDetail.as_view(), name='univalluno-detail'),
+
+    # Vistas CRUD para ArticuloDeportivo
+    path('articulo/', views.ArticuloDeportivoList.as_view(), name='articulo-list'),
+    path('articulo/<int:pk>/', views.ArticuloDeportivoDetail.as_view(), name='articulo-detail'),
+
+    # Vistas CRUD para Prestamo
+    path('prestamo/', views.PrestamoList.as_view(), name='prestamo-list'),
+    path('prestamo/<int:pk>/', views.PrestamoDetail.as_view(), name='prestamo-detail'),
+
+    # Vistas CRUD para Multa
+    path('multa/', views.MultaList.as_view(), name='multa-list'),
+    path('multa/<int:pk>/', views.MultaDetail.as_view(), name='multa-detail'),
+
+    # Vistas especiales
+    path('generar-multas/', views.generar_multas_view, name='generar-multas'),
+    path('reporte-deporte/', views.ReporteArticulosPorDeporte.as_view(), name='reporte-deporte'),
+    path('reporte-dia/', views.ReporteArticulosPorDia.as_view(), name='reporte-dia'),
+    path('datos-articulos/', views.datos_articulos_por_deporte, name='datos-articulos'),
+    
+    path('reportes/', views.reportes_view, name='reportes'),
 
 ]
+
 
 
 
